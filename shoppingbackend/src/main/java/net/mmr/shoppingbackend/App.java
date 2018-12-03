d29aa54759431d0027c5242cd0a5a1c110f61a36
mahdimmr <mahdi.rezayi76@gmail.com>
2018-12-03 16:08:42 +0330

    My Project Setup Completed Successfully.

---
+++ shoppingbackend/src/main/java/net/mmr/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.mmr.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
